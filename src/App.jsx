b5d51e82c3fd11d1985cd8d0936b506de5860ac5
0baid <obaidurrehman555@gmail.com>
2024-12-10 15:54:29 +0500

    testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero'
 import Features from './sections/Features'
 import Pricing from './sections/Pricing'
 import FAQ from './sections/FAQ'
+import Testimonials from './sections/Testimonials'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
         <Features/>
         <Pricing/>
         <FAQ/>
+        <Testimonials/>
       </main>
     
      
